94ef5b88d27c0995604a6d7f1885591e39f21526
Ricard_MBP16 <rikitzzz@gmail.com>
2020-11-08 10:53:24 +0100

    test: test for login and get_filings
    Also added the corresponding features.
    
    Still connecting to backend. Need to add mocking.

--- pyfinance/finten.py
+++ pyfinance/finten.py
@@ -1,9 +1,17 @@
 import requests
+import pandas as pd
 
+class InvalidCredentials(Exception):
+  pass
 
 class FinTen:
     def __init__(self):
         self.URI = "https://finten.weirwood.ai"
+        self.LOGIN_URI = self.URI + "/api/users/login"
+        self.FILINGS_URI = self.URI + "/api/company/filings"
+        self._token = None
+        self._username = None
+        self._password = None
 
     def _is_reachable(self) -> bool:
         response = requests.get(self.URI)
@@ -14,6 +22,38 @@ class FinTen:
                     return True
             except Exception as e:
                 print(f"Unable to reach FinTen API {e}")
-                return False
         return False
+
+    def set_login(self, username, password):
+      self._username = username
+      self._password = password
+      self._token = None
+
+    def _login(self):
+      if (self._username == None) or (self._password == None):
+        raise InvalidCredentials("username or password has not been set!")
+
+      payload = f"username={self._username}&password={self._password}"
+      headers = {
+        "Content-Type": "application/x-www-form-urlencoded"
+      }
+      response = requests.post(self.LOGIN_URI, headers = headers, data = payload)
+
+      if response.status_code != 200:
+        raise InvalidCredentials
+
+      self._token = response.json()["token"]
+
+    def get_filings(self, ticker):
+      if self._token == None:
+        self._login()
+
+      headers = {
+        "Authorization": f"Bearer {self._token}"
+      }
+
+      response = requests.get(self.FILINGS_URI+'?ticker='+ticker,headers=headers, data={})
+
+      return pd.DataFrame(response.json()["filings"])
+
 
--- tests/test_finten.py
+++ tests/test_finten.py
@@ -1,6 +1,12 @@
 import pytest
-from pyfinance.finten import FinTen
+from pyfinance.finten import FinTen, InvalidCredentials
+import json
 
+@pytest.fixture
+def finten_login():
+  with open("./credentials.json") as f:
+    finten_login = json.load(f)
+  return finten_login
 
 def test_is_reachable():
     finten = FinTen()
@@ -8,8 +14,24 @@ def test_is_reachable():
     finten.URI = "https://www.google.com"
     assert finten._is_reachable() == False
 
+def test_login__no_login_set():
+    with pytest.raises(InvalidCredentials):
+      finten = FinTen() 
+      finten._login()
 
-def test_login():
+def test_login__invalid_credentials():
+  with pytest.raises(InvalidCredentials):
     finten = FinTen()
-    finten.set_key(API_KEY="foo")
+    finten.set_login(username = "foo", password = "bar")
+    finten._login()
 
+def test_login(finten_login):
+  finten = FinTen()
+  finten.set_login(**finten_login)
+  finten._login()
+
+def test_get_filings(finten_login):
+  finten = FinTen()
+  finten.set_login(**finten_login)
+  filings = finten.get_filings(ticker="AAPL")
+  assert len(filings) > 0
