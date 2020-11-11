c1ee251734699663ff7e5e0f4935ed087928fc50
Àlex Encalado Masià <faraday@MacBook-MacBook-Pro-de-Alex.local>
2020-11-11 20:47:57 +0100

    test: Added test cases
    Added default login in FinTen and it works fine with get_filings method

--- tests/test_finten.py
+++ tests/test_finten.py
@@ -1,12 +1,15 @@
 import pytest
 from pyfinance.finten import FinTen, InvalidCredentials
 import json
+import httpretty
+
 
 @pytest.fixture
 def finten_login():
-  with open("./.credentials.json") as f:
-    finten_login = json.load(f)
-  return finten_login
+    with open("./.credentials.json") as f:
+        finten_login = json.load(f)
+    return finten_login
+
 
 def test_is_reachable():
     finten = FinTen()
@@ -14,24 +17,54 @@ def test_is_reachable():
     finten.URI = "https://www.google.com"
     assert finten._is_reachable() == False
 
+
 def test_login__no_login_set():
-    with pytest.raises(InvalidCredentials):
-      finten = FinTen() 
-      finten._login()
+    finten = FinTen()
+    assert finten._token == None
+    finten._login()
+    assert finten._username == "pyfinance"
+    assert finten._password == "pyfinance"
+    assert finten._token != None
+
 
 def test_login__invalid_credentials():
-  with pytest.raises(InvalidCredentials):
-    finten = FinTen()
-    finten.set_login(username = "foo", password = "bar")
-    finten._login()
+    with pytest.raises(InvalidCredentials):
+        finten = FinTen()
+        finten.set_login(username="foo", password="bar")
+        finten._login()
+
 
 def test_login(finten_login):
-  finten = FinTen()
-  finten.set_login(**finten_login)
-  finten._login()
+    finten = FinTen()
+    finten.set_login(**finten_login)
+    finten._login()
+
 
 def test_get_filings(finten_login):
-  finten = FinTen()
-  finten.set_login(**finten_login)
-  filings = finten.get_filings(ticker="AAPL")
-  assert len(filings) > 0
+    finten = FinTen()
+    finten.set_login(**finten_login)
+    filings = finten.get_filings(ticker="AAPL")
+    assert len(filings) > 0
+
+
+def test_get_filings_with_public_login():
+    httpretty.enable()
+    httpretty.register_uri(
+        httpretty.GET,
+        "https://finten.weirwood.ai/api/company/filings?ticker=AAPL",
+        body='{"filings": [{"foo": "bar", "manchu": "masachussets"}]}',
+    )
+    httpretty.register_uri(
+        httpretty.POST,
+        "https://finten.weirwood.ai/api/users/login",
+        body='{"token": "test"}',
+    )
+
+    filings = FinTen().get_filings(ticker="AAPL")
+    assert len(filings) > 0
+
+
+def test_get_prices():
+    aapl = FinTen().get_prices(ticker="AAPL")
+    assert len(aapl) > 0
+
